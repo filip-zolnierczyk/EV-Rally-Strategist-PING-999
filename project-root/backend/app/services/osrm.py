74b06de37890f79ef9d898ed1d9ddace132697bf
witNie <witold.niec@gmail.com>
2026-03-28 21:39:52 +0100

    fixed coordinates type

--- project-root/backend/app/services/osrm.py
+++ project-root/backend/app/services/osrm.py
@@ -1,4 +1,4 @@
-from typing import List
+from typing import List, Tuple
 from math import inf
 
 import requests
@@ -6,13 +6,23 @@ import requests
 OSRM_BASE_URL = "http://router.project-osrm.org/route/v1/driving/"
 
 # lat,lon -> lon,lat
-def convert_to_lonlat(position : str) -> str:
-    result = "".join(position.split())
-    lat, lon = result.split(",")
-    return lon + "," + lat
+def convert_to_lonlat(position : Tuple[float, float]) -> Tuple[float, float]:
+    lat, lon = position
+    return (lon, lat)
 
-def calculate_distance(start : str, end : str):
-    url = f"{OSRM_BASE_URL}/{start};{end}"
+def coordinates_to_str(coordinates : Tuple[float, float]) -> str:
+    lon, lat = coordinates
+    return str(lon) + "," + str(lat)
+
+def coordinates_to_tuple(coordinates : str) -> Tuple[float, float]:
+    result = "".join(coordinates.split())
+    lat, lon = result.split(",")
+    return (float(lon), float(lat))
+
+def calculate_distance(start : Tuple[float, float], end : Tuple[float, float]):
+    start_s = coordinates_to_str(start)
+    end_s = coordinates_to_str(end)
+    url = f"{OSRM_BASE_URL}/{start_s};{end_s}"
     params = {
         "overview": "full",
         "geometries": "geojson"
@@ -27,10 +37,11 @@ def calculate_distance(start : str, end : str):
 
     return distance_km, duration_min
 
-def calculate_route(waypoints : List[str]):
+def calculate_route(waypoints : List[Tuple[float, float]]):
     url = f"{OSRM_BASE_URL}"
     for waypoint in waypoints:
-        url = f"{url}{waypoint};"
+        waypoint_s = coordinates_to_str(waypoint)
+        url = f"{url}{waypoint_s};"
     url = url[:-1]
 
     params = {
@@ -47,7 +58,7 @@ def calculate_route(waypoints : List[str]):
     return distance_km, duration_min
 
 
-def optimize_distance_with_charging(start : str, end : str, chargers : List[str]):
+def optimize_distance_with_charging(start : Tuple[float, float], end : Tuple[float, float], chargers : List[Tuple[float, float]]):
     # for now testing routing with only one charging, only for api testing, will change it asap
     # optimizing for time, I guess it could be a parameter to optimize time vs distance idk
     current_duration = inf
@@ -67,7 +78,11 @@ if __name__ == "__main__":
     start = "19.9450,50.0647"
     end = "21.0122,52.2297"
 
+    start = coordinates_to_tuple(start)
+    end = coordinates_to_tuple(end)
+
     chargers = ["51.360542, 21.130188", "51.067327 , 20.841432", "51.620237 , 20.974218 "]
+    chargers = list(map(coordinates_to_tuple, chargers))
     chargers = list(map(convert_to_lonlat, chargers))
     direct_distance, direct_duration = calculate_distance(start, end)
     print(f"Direct distance: {direct_distance:.2f} km")
